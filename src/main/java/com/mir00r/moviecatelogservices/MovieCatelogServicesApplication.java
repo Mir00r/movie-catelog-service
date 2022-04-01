20270af28fa76411f39b44fee741a5ff6672f525
razzak007 <razzak@cmedhealth.com>
2022-04-01 17:09:14 +0600

    ïnitiate spring boot project for movie catelog service

---
+++ src/main/java/com/mir00r/moviecatelogservices/MovieCatelogServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mir00r.moviecatelogservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatelogServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatelogServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mir00r/moviecatelogservices/MovieCatelogServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mir00r.moviecatelogservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatelogServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
